d948876e8baaf043930e9c3d2dd6222638d260d8
Luky20 <lukywilan@gmail.com>
2024-09-16 21:52:46 +0700

    whatweplay

--- app/src/main/java/com/ubaya/nmp_esports/PlayActivity.kt
+++ app/src/main/java/com/ubaya/nmp_esports/PlayActivity.kt
@@ -1,20 +1,25 @@
 package com.ubaya.nmp_esports
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ubaya.nmp_esports.databinding.ActivityInformationBinding
+import com.ubaya.nmp_esports.databinding.ActivityPlayBinding
 
 class PlayActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_play2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityPlayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, HomeFragment::class.java)
+            startActivity(intent)
         }
     }
 }
