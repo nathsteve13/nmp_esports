de1645df00b800fbf9e539ea39bf7dc3a0d417e9
nathsteve13 <nathanaelsteven06@gmail.com>
2024-09-16 18:46:50 +0700

    information activity done

--- app/src/main/java/com/ubaya/nmp_esports/InformationActivity.kt
+++ app/src/main/java/com/ubaya/nmp_esports/InformationActivity.kt
@@ -1,20 +1,25 @@
 package com.ubaya.nmp_esports
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ubaya.nmp_esports.databinding.ActivityInformationBinding
 import com.ubaya.nmp_esports.databinding.FragmentHomeBinding
 
 class InformationActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: ActivityInformationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = ActivityInformationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.backHomeBtn.setOnClickListener {
+            val intent = Intent(this, HomeFragment::class.java)
+            startActivity(intent)
+        }
     }
 }
