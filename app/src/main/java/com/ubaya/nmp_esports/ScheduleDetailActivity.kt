727f9991f4be3b70cc3c910a7320866b0243e91d
nathsteve13 <nathanaelsteven06@gmail.com>
2024-09-17 17:44:59 +0700

    data distribution from schedule to schedule detail

---
+++ app/src/main/java/com/ubaya/nmp_esports/ScheduleDetailActivity.kt
@@ -0,0 +1,17 @@
+package com.ubaya.nmp_esports
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ubaya.nmp_esports.databinding.ActivityInformationBinding
+
+class ScheduleDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInformationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/ubaya/nmp_esports/ScheduleFragment.kt
+++ app/src/main/java/com/ubaya/nmp_esports/ScheduleFragment.kt
@@ -1,11 +1,14 @@
 package com.ubaya.nmp_esports
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.ubaya.nmp_esports.databinding.FragmentHomeBinding
+import com.ubaya.nmp_esports.databinding.FragmentScheduleBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +20,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ScheduleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class ScheduleFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentScheduleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +38,42 @@ class ScheduleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        binding = FragmentScheduleBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.btnSch1.setOnClickListener {
+            val scheduleTitle = "Open Winter Cup - Valorant"
+            val scheduleTime = "09-09-2024 13.00 PM"
+
+            val intent = Intent(activity, ScheduleDetailActivity::class.java)
+            intent.putExtra(SCHEDULE_TITLE, scheduleTitle)
+            intent.putExtra(SCHEDULE_TIME, scheduleTime)
+            startActivity(intent)
+        }
+
+        binding.btnSch2.setOnClickListener {
+            val scheduleTitle = "Open Winter Cup - Mobile Legend"
+            val scheduleTime = "09-09-2024 13.00 PM"
+
+            val intent = Intent(activity, ScheduleDetailActivity::class.java)
+            intent.putExtra(SCHEDULE_TITLE, scheduleTitle)
+            intent.putExtra(SCHEDULE_TIME, scheduleTime)
+            startActivity(intent)
+        }
+
+        binding.btnSch3.setOnClickListener {
+            val scheduleTitle = "Open Winter Cup - Fortnite"
+            val scheduleTime = "09-09-2024 13.00 PM"
+
+            val intent = Intent(activity, ScheduleDetailActivity::class.java)
+            intent.putExtra(SCHEDULE_TITLE, scheduleTitle)
+            intent.putExtra(SCHEDULE_TIME, scheduleTime)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,6 +86,9 @@ class ScheduleFragment : Fragment() {
     }
 
     companion object {
+        const val SCHEDULE_TITLE = "schedule_title"
+        const val SCHEDULE_TIME = "schedule_time"
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -56,7 +97,6 @@ class ScheduleFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ScheduleFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ScheduleFragment().apply {
