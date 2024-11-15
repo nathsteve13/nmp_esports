be4437b1f9a12818158a154506d7340603640a87
Luky20 <lukywilan@gmail.com>
2024-11-15 12:23:56 +0700

    layout with view pager

--- app/src/main/java/com/ubaya/nmp_esports/MenuActivity.kt
+++ app/src/main/java/com/ubaya/nmp_esports/MenuActivity.kt
@@ -7,37 +7,39 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager2.widget.ViewPager2
 import com.ubaya.nmp_esports.databinding.ActivityMenuBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 class MenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMenuBinding
+    val fragments: ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(PlayFragment())
-
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.whatweplay -> {
-                    replaceFragment(PlayFragment())
-                }
-                R.id.whoweare -> {
-                    replaceFragment(WhoWeAreFragment())
-                }
-                R.id.schedule -> {
-                    replaceFragment(ScheduleFragment())
-                }
+        fragments.add(PlayFragment())
+        fragments.add(WhoWeAreFragment())
+        fragments.add(ScheduleFragment())
+        binding.viewPager.adapter = MyAdapter(this, fragments)
+        binding.viewPager.registerOnPageChangeCallback(object:
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.bottomNavigationView.selectedItemId =
+                    binding.bottomNavigationView.menu.getItem(position).itemId
+            }
+        }
+        )
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            binding.viewPager.currentItem = when(it.itemId) {
+                R.id.whatweplay -> 0
+                R.id.whoweare -> 1
+                R.id.schedule -> 2
+                else -> 0 // default to home
             }
             true
         }
     }
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
-    }
+
 }
